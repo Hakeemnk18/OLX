8d42838305e5ef80dc4aea11d3aa4120b0f96de6
Hakeem Muhammed <hakeemnk18@gmail.com>
2025-03-12 01:10:27 +0530

    home page created

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import Header from '../Components/Header'
 import Banner from '../Components/Banner'
+import Posts from '../Components/Posts'
+import Footer from '../Components/Footer'
 
 const Home = () => {
   return (
     <div>
       <Header />
       <Banner />
+      <Posts />
+      <Footer />
     </div>
   )
 }
