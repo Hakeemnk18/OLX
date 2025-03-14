8d7f516d61dce94e98d9ed70a6dde284544a3d32
Hakeem Muhammed <hakeemnk18@gmail.com>
2025-03-14 22:14:07 +0530

    products show dynamicaly

--- src/Components/Posts.jsx
+++ src/Components/Posts.jsx
@@ -1,9 +1,34 @@
 import React from 'react';
-import {useEffect,useContext,useState} from 'react';
+import {useEffect,useState,useContext} from 'react';
 import Heart from '../assets/Heart';
 import './Posts.css';
+import { FireBaseContext } from '../store/FirbaseContext';
+import { collection, getDoc, getDocs } from 'firebase/firestore';
+
 
 function Posts() {
+
+  const{auth,db} = useContext(FireBaseContext)
+  const [products,setProducts] = useState([])
+
+  useEffect(()=>{
+    getProduct()
+  },[])
+
+  const getProduct = async()=>{
+    const snapShot = await getDocs(collection(db,'product'))
+    
+    const productList = snapShot.docs.map((product)=>{
+      return {
+        ...product.data(),
+        id:product.id
+      }
+    })
+    setProducts(productList)
+    //console.log(productList)
+  }
+
+  
 
   return (
     <div className="postParentDiv">
@@ -13,24 +38,33 @@ function Posts() {
           <span>View more</span>
         </div>
         <div className="cards">
-          <div
-            className="card"
-          >
-            <div className="favorite">
-              <Heart></Heart>
-            </div>
-            <div className="image">
-              <img src="../../../Images/R15V3.jpg" alt="" />
-            </div>
-            <div className="content">
-              <p className="rate">&#x20B9; 250000</p>
-              <span className="kilometer">Two Wheeler</span>
-              <p className="name"> YAMAHA R15V3</p>
-            </div>
-            <div className="date">
-              <span>Tue May 04 2021</span>
-            </div>
-          </div>
+
+        {
+          products.map((product)=>{
+            return (
+              <div
+                className="card"
+              >
+                <div className="favorite">
+                  <Heart></Heart>
+                </div>
+                <div className="image">
+                  <img src={product.image_url} alt="" />
+                </div>
+                <div className="content">
+                  <p className="rate">&#x20B9; {product.price}</p>
+                  <span className="kilometer">{product.category}</span>
+                  <p className="name"> {product.name}</p>
+                </div>
+                <div className="date">
+                  <span>{product.createdAt.slice(4,15)}</span>
+                </div>
+              </div>
+            )
+          })
+        }
+          
+
         </div>
       </div>
       <div className="recommendations">
